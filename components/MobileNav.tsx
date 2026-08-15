8490ab8b39cb88ea5656552fbae47a3529d5bc7d
labiblianoshablapodcast-gif <labiblianoshablapodcast@gmail.com>
2026-08-15 13:21:23 -0400

    Add gallery tab to mobile navigation

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -4,6 +4,7 @@ export default function MobileNav(){
   return <nav className="mobileBottomNav" aria-label="Navegación móvil">
     <Link href="/"><span>⌂</span><small>Inicio</small></Link>
     <Link href="/biblia"><span>📖</span><small>Biblia</small></Link>
+    <Link href="/galeria"><span>▣</span><small>Galería</small></Link>
     <Link href="/conexion"><span>♢</span><small>Peticiones</small></Link>
     <Link href="/donaciones"><span>♡</span><small>Donar</small></Link>
   </nav>;
